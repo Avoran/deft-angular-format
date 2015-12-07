6c69af8cf2b8d84d732d2dcc7949fd846db91a04
Pieter Verloop <pieter@deft.nl>
2015-12-07 14:34:46 +0100

    Fix directive dependency injections. Bump to 1.0.1

--- src/directive.js
+++ src/directive.js
@@ -1,15 +1,15 @@
 angular.module('deft.format', [])
-  .factory('Formatter', function() {
+  .factory('DeftFormatter', function() {
     return { get: function(format) { return new Formatter(format); } }
   })
-  .directive('deftFormat', function () {
+  .directive('deftFormat', function (DeftFormatter) {
     return {
       restrict: 'A',
       require: '?ngModel',
       scope: {format:'@deftFormat' },
-      link: function (scope, element, attrs, ngModel, Formatter) {
+      link: function (scope, element, attrs, ngModel) {
         if (!ngModel) return;
-        var formatter = Formatter.get(scope.format);
+        var formatter = DeftFormatter.get(scope.format);
         ngModel.$parsers.unshift(function (inputValue) {
           return formatter.format(inputValue);
         });
